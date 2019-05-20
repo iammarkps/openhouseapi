828cf5e36a8bef638bb263efbe34691bb33eff8a
iammarkps <markkung08@gmail.com>
2019-05-20 13:10:40 +0700

    JSONtoCSV

--- functions/index.js
+++ functions/index.js
@@ -34,3 +34,33 @@ exports.registration = functions
   .region('asia-northeast1')
   .runWith({ memory: '1GB' })
   .https.onRequest(app)
+
+const json2csv = require('json2csv').parse
+
+exports.csvJsonReport = functions
+  .region('asia-northeast1')
+  .runWith({ memory: '1GB' })
+  .https.onRequest((request, response) => {
+    const db = admin.firestore()
+    const registrantRef = db.collection('registrant')
+    return registrantRef
+      .get()
+      .then(querySnapshot => {
+        const orders = []
+
+        querySnapshot.forEach(doc => {
+          const order = doc.data()
+          orders.push(order)
+        })
+        const csv = json2csv(orders)
+        response.setHeader(
+          'Content-disposition',
+          'attachment; filename=report.csv'
+        )
+        response.set('Content-Type', 'text/csv')
+        response.status(200).send(csv)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
